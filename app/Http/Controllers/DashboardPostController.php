4fefbe0282c0c3c9455b15483cdca60734463111
saiifulhilal <hilalasw@gmail.com>
2023-11-14 10:27:55 +0700

    tugas video 22 Preview, update, & delete image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 
@@ -87,15 +88,24 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image'=> 'image|file|max:1024',
             'body' => 'required'
         ];
 
+        
         if($request->slug != $post->slug)
         {
             $rules['slug'] = 'required|unique:posts';
         }
-
+        
         $validateData = $request->validate($rules);
+
+        if($request->file('image')) {
+            if($request->oldImage){
+                Storage::delete($request->oldImage);
+            }
+            $validateData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validateData['user_id'] = auth()->user()->id;
         $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -111,7 +121,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        
+        if($post->image){
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post has been Deleted!');
--- resources/views/Dashboard/layouts/header.blade.php
+++ resources/views/Dashboard/layouts/header.blade.php
@@ -1,10 +1,10 @@
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+<header class="navbar navbar-dark navbar-expand-lg sticky-top bg-dark flex-md-nowrap p-1 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/dashboard">Hail Hilal | Blog</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-    <div class="navbar-nav">
+    <input class="form-control form-control-dark w-50" type="text" placeholder="Search" aria-label="Search">
+    {{-- <div class="navbar-nav">
       <div class="nav-item text-nowrap">
         <form action="/logout" method="post">
           @csrf
@@ -13,5 +13,29 @@
           </button>
       </form>
       </div>
-    </div>
+    </div> --}}
+    <ul class="navbar-nav ms-auto">
+      @auth
+      <li class="nav-item dropdown px-5">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Welcome, {{ auth()->user()->name }}
+        </a>
+        <ul class="dropdown-menu px-3 border-0" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="/"><span data-feather="home"></span> Home</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li>
+            <form action="/logout" method="post">
+                @csrf
+                <button type="submit" class="dropdown-item">
+                <span data-feather="log-out"></span> Logout
+                </button>
+            </form>
+        </ul>
+      </li>
+      @else
+        <li class="nav-item">
+            <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+        </li>
+        @endauth
+    </ul>
   </header>
